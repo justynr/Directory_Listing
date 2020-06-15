c58e256ed79b9f49557d2cca14af1162b253d22b
justynr <justynr@me.com>
2020-06-15 12:35:51 -0400

    JavaScript - Static data

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,539 @@
-//https://docs.google.com/spreadsheets/d/14-mFjnNsscxauPxJgMZxTFtrrc319h3akb1fS6P27HY/edit?usp=sharing
+const public_spreadsheet_url_suites = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNik9F_6vPbPKCGneA6qMgExrkJhyl6MrzJhVj8PrK_T_EqqrFsgFJtPALhGkSw6mnX_nSuhknqZ0z/pubhtml?gid=0&single=true';
+const public_spreadsheet_url_master = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNik9F_6vPbPKCGneA6qMgExrkJhyl6MrzJhVj8PrK_T_EqqrFsgFJtPALhGkSw6mnX_nSuhknqZ0z/pubhtml?gid=941655149&single=true';
+const public_spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTNik9F_6vPbPKCGneA6qMgExrkJhyl6MrzJhVj8PrK_T_EqqrFsgFJtPALhGkSw6mnX_nSuhknqZ0z/pubhtml";
+
+const masterData = [
+  {
+    "category":"Spa",
+    "color":"purple"
+  },
+  {
+    "category":"Other",
+    "color":"pink"
+  },
+  {
+    "category":"Salon",
+    "color":"red"
+  },
+  {
+    "category":"Barber",
+    "color":"blue"
+  },
+  {
+    "category":"Chiro",
+    "color":"orange"
+  },
+
+]
+
+const data = [
+  {
+    "suite": 1,
+    "ownerFirst": "Stavro",
+    "ownerLast": "Piniotes",
+    "email": "hairbystavro@gmail.com",
+    "phone": "416-897-4813",
+    "companyName": "Hair By Stavro",
+    "category": "Salon"
+  },
+  {
+    "suite": 2,
+    "ownerFirst": "Myrha",
+    "ownerLast": "Foutris",
+    "email": "hbe416939@gmail.com",
+    "phone": "416-939-1423",
+    "companyName": "HBE Med Spa",
+    "category": "Spa"
+  },
+  {
+    "suite": 4,
+    "ownerFirst": "Nikki",
+    "ownerLast": "Bailey",
+    "email": "dollhousestudio.xo@gmail.com",
+    "phone": "647-336-3655",
+    "companyName": "Dollhouse",
+    "category": "Salon"
+  },
+  {
+    "suite": 4,
+    "ownerFirst": "Cree",
+    "ownerLast": "Barrocks",
+    "email": "info@creebarrocks.com",
+    "phone": "647-567-3007",
+    "companyName": "Kuhler",
+    "category": "Salon, Barber"
+  },
+  {
+    "suite": 5,
+    "ownerFirst": "Anastasia",
+    "ownerLast": "Chondrogiannis",
+    "email": "anastasiachondro2@gmail.com",
+    "phone": "416-889-4568",
+    "companyName": "Kalon Hair Society",
+    "category": "Salon"
+  },
+  {
+    "suite": 6,
+    "ownerFirst": "Ania",
+    "ownerLast": "Wazny",
+    "email": "bookings@aniawaznycreative.com",
+    "phone": "416-908-7436",
+    "companyName": "Ania Wazny Creative",
+    "category": "Salon"
+  },
+  {
+    "suite": 7,
+    "ownerFirst": "Krista",
+    "ownerLast": "Dolmon",
+    "email": "skinstudiotoronto@gmail.com",
+    "phone": "n/a",
+    "companyName": "Skin Studio",
+    "category": "Spa"
+  },
+  {
+    "suite": 8,
+    "ownerFirst": "Tatiana",
+    "ownerLast": "Gamboa",
+    "email": "tatianaprofessionalstylist@gmail.com",
+    "phone": "416-919-6005",
+    "companyName": "Hair Addictz Studio",
+    "category": "Salon"
+  },
+  {
+    "suite": 9,
+    "ownerFirst": "Giselle",
+    "ownerLast": "Sarmiento",
+    "email": "giselleantonio18@gmail.com",
+    "phone": "647-210-6101",
+    "companyName": "Giselle Joy Studio",
+    "category": "Salon"
+  },
+  {
+    "suite": 10,
+    "ownerFirst": "Sonia",
+    "ownerLast": "Roshi",
+    "email": "soilaroshi@yahoo.com",
+    "phone": "647-785-8867",
+    "companyName": "Hair By Sonia",
+    "category": "Salon"
+  },
+  {
+    "suite": 11,
+    "ownerFirst": "Christina",
+    "ownerLast": "White",
+    "email": "christine.stylist@gmail.com",
+    "phone": "647-221-7251",
+    "companyName": "Chroma Salon",
+    "category": "Salon"
+  },
+  {
+    "suite": 12,
+    "ownerFirst": "Tre",
+    "ownerLast": "Paul",
+    "email": "designertrepaul@gmail.com",
+    "phone": "416-824-9298",
+    "companyName": "Ocean Wavy Barbers",
+    "category": "Barber"
+  },
+  {
+    "suite": 13,
+    "ownerFirst": "Dixie",
+    "ownerLast": "Burke",
+    "email": "naturalcurlhaven@gmail.com",
+    "phone": "416-892-4022",
+    "companyName": "Natural Curly Hair Haven",
+    "category": "Salon"
+  },
+  {
+    "suite": 14,
+    "ownerFirst": "Christina",
+    "ownerLast": "Collini",
+    "email": "browblvdinc@gmail.com",
+    "phone": "647-524-9988",
+    "companyName": "Brow Blvd Inc",
+    "category": "Spa"
+  },
+  {
+    "suite": 14,
+    "ownerFirst": "Christina",
+    "ownerLast": "Vafin",
+    "email": "christinavafin@gmail.com",
+    "phone": "437-986-1984",
+    "companyName": "Effect MedAesthtics Centre",
+    "category": "Spa"
+  },
+  {
+    "suite": 15,
+    "ownerFirst": "Yvonne",
+    "ownerLast": "Pine",
+    "email": "info@hairillusions.ca",
+    "phone": "416-846-4660",
+    "companyName": "Hair Illusions",
+    "category": "Salon"
+  },
+  {
+    "suite": 16,
+    "ownerFirst": "Paul",
+    "ownerLast": "Chan",
+    "email": "paulkitchan@hotmail.ca",
+    "phone": "416-888-1726",
+    "companyName": "Raw Hair Studio",
+    "category": "Salon"
+  },
+  {
+    "suite": 18,
+    "ownerFirst": "Katina",
+    "ownerLast": "Hasselsteiner",
+    "email": "big6lashes@gmail.com",
+    "phone": "416-788-9161",
+    "companyName": "Big 6 Lashes",
+    "category": "Other"
+  },
+  {
+    "suite": 19,
+    "ownerFirst": "Catherine",
+    "ownerLast": "Furness",
+    "email": "cfurness@hotmail.ca",
+    "phone": "416-795-0937",
+    "companyName": "Advanced Skin Care",
+    "category": "Spa"
+  },
+  {
+    "suite": 20,
+    "ownerFirst": "Amanpreet",
+    "ownerLast": "Banwait",
+    "email": "aman_banwait@hotmail.ca",
+    "phone": "416-904-6653",
+    "companyName": "Let Them Notice",
+    "category": "Spa"
+  },
+  {
+    "suite": 20,
+    "ownerFirst": "Sarah",
+    "ownerLast": "Harrison",
+    "email": "truebeautylounge@gmail.com",
+    "phone": "416-528-9361",
+    "companyName": "True Beauty Lounge",
+    "category": "Spa"
+  },
+  {
+    "suite": 21,
+    "ownerFirst": "Keyla",
+    "ownerLast": "Letanno",
+    "email": "keylaletanno@gmail.com",
+    "phone": "647-207-6338",
+    "companyName": "Key To Beauty",
+    "category": "Spa"
+  },
+  {
+    "suite": 22,
+    "ownerFirst": "Jeongae",
+    "ownerLast": "Davidson",
+    "email": "jeongae77@rogers.ca",
+    "phone": "647-527-4132",
+    "companyName": "Eye The Brows",
+    "category": "Other"
+  },
+  {
+    "suite": 23,
+    "ownerFirst": "Kimberli",
+    "ownerLast": "Miou",
+    "email": "kimberli@nqwb.ca",
+    "phone": "647-704-8857",
+    "companyName": "North Queen Wellness & Beauty",
+    "category": "Other, Spa"
+  },
+  {
+    "suite": 25,
+    "ownerFirst": "Julia",
+    "ownerLast": "Michener",
+    "email": "juliammichener@gmail.com",
+    "phone": "647-222-7293",
+    "companyName": "Julia Skin Beauty",
+    "category": "Spa, Other"
+  },
+  {
+    "suite": 26,
+    "ownerFirst": "Francine",
+    "ownerLast": "Francis",
+    "email": "francinefrancis_lee@yahoo.ca",
+    "phone": "416-725-8710",
+    "companyName": "Virtuous Hair",
+    "category": "Salon"
+  },
+  {
+    "suite": 27,
+    "ownerFirst": "Melissa",
+    "ownerLast": "Gagnon",
+    "email": "m.gagnon1987@hotmail.ca",
+    "phone": "647-987-6145",
+    "companyName": "Canadian Beauty Boutique",
+    "category": "Spa"
+  },
+  {
+    "suite": 28,
+    "ownerFirst": "Emma",
+    "ownerLast": "Toste",
+    "email": "nailgoddessacademy@gmail.com",
+    "phone": "647-570-0763",
+    "companyName": "Nail Goddess Academy",
+    "category": "Other"
+  },
+  {
+    "suite": 29,
+    "ownerFirst": "Taran",
+    "ownerLast": "Chohan",
+    "email": "tarandip.chohan@mohawkcollege.ca",
+    "phone": "647-973-6025",
+    "companyName": "Refined By T",
+    "category": "Spa"
+  },
+  {
+    "suite": 29,
+    "ownerFirst": "Ashley",
+    "ownerLast": "Tavares",
+    "email": "ashleyjtxo@gmail.com",
+    "phone": "437-993-3731",
+    "companyName": "Skin From Within",
+    "category": "Spa"
+  },
+  {
+    "suite": 30,
+    "ownerFirst": "Areta",
+    "ownerLast": "Mamaduvoski",
+    "email": "aretam_hairlove@outlook.com",
+    "phone": "647-500-2157",
+    "companyName": "Virtue Hair",
+    "category": "Salon"
+  },
+  {
+    "suite": 30,
+    "ownerFirst": "Eyeza",
+    "ownerLast": "Diana",
+    "email": "justsmile.designs@gmail.com",
+    "phone": "647-468-2173",
+    "companyName": "Just Smile Designs",
+    "category": "Salon, Spa"
+  },
+  {
+    "suite": 31,
+    "ownerFirst": "Ashley",
+    "ownerLast": "Bell",
+    "email": "ashleyvbell88@gmail.com",
+    "phone": "905-537-0820",
+    "companyName": "Ashley Bell Hairstylist",
+    "category": "Salon"
+  },
+  {
+    "suite": 33,
+    "ownerFirst": "Justine",
+    "ownerLast": "Mitchell",
+    "email": "info@amethystoasis.ca",
+    "phone": "905-866-3621",
+    "companyName": "Amethyst Oasis",
+    "category": "Spa"
+  },
+  {
+    "suite": 34,
+    "ownerFirst": "Barbara",
+    "ownerLast": "Brown",
+    "email": "all_in_one_beauty@yahoo.com",
+    "phone": "647-219-0024",
+    "companyName": "All In One Beauty",
+    "category": "Salon"
+  },
+  {
+    "suite": 35,
+    "ownerFirst": "Lesia",
+    "ownerLast": "Mcpherson",
+    "email": "lesia.mcpherson@hotmail.com",
+    "phone": "647-280-6722",
+    "companyName": "Pluss Hair",
+    "category": "Salon"
+  },
+  {
+    "suite": 36,
+    "ownerFirst": "Lindsay",
+    "ownerLast": "Fiander",
+    "email": "info@glossstudio.ca",
+    "phone": "647-922-8664",
+    "companyName": "Gloss Studio",
+    "category": "Salon"
+  },
+  {
+    "suite": 38,
+    "ownerFirst": "Jennifer",
+    "ownerLast": "Bauch",
+    "email": "jenniferhairdesign@yahoo.com",
+    "phone": "416-725-5366",
+    "companyName": "Jennifer Hair Design",
+    "category": "Salon"
+  },
+  {
+    "suite": 40,
+    "ownerFirst": "Diane",
+    "ownerLast": "Carreiro",
+    "email": "diane.merakistudio@gmail.com",
+    "phone": "416-333-1100",
+    "companyName": "Meraki Hair Studio",
+    "category": "Salon"
+  },
+  {
+    "suite": 41,
+    "ownerFirst": "Lola",
+    "ownerLast": "Fakule",
+    "email": "lola.elmai@gmail.com",
+    "phone": "647-210-2979",
+    "companyName": "El'mai Studio",
+    "category": "Salon, Other"
+  },
+  {
+    "suite": 41,
+    "ownerFirst": "James",
+    "ownerLast": "Han",
+    "email": "barberjhan@gmail.com",
+    "phone": "647-787-2833",
+    "companyName": "Evolve",
+    "category": "Barber"
+  },
+  {
+    "suite": 43,
+    "ownerFirst": "Sandra",
+    "ownerLast": "Mackenzie",
+    "email": "Ssandrameuiller@gmail.com",
+    "phone": "416-617-1624",
+    "companyName": "Sandra Mackenzie Design",
+    "category": "Salon"
+  },
+  {
+    "suite": 44,
+    "ownerFirst": "Gianna",
+    "ownerLast": "Soncina",
+    "email": "drsoncina@gmail.com",
+    "phone": "416-400-2752",
+    "companyName": "Gianna Chiropractic",
+    "category": "Chiro"
+  },
+  {
+    "suite": 44,
+    "ownerFirst": "Sharon",
+    "ownerLast": "Jeremih",
+    "email": "sharonj.rmt@gmail.com",
+    "phone": "647-281-5170",
+    "companyName": "Sharon Massage Therapy",
+    "category": "Other"
+  },
+  {
+    "suite": 45,
+    "ownerFirst": "Angela",
+    "ownerLast": "Bianca",
+    "email": "angbianca1@hotmail.com",
+    "phone": "647-525-8409",
+    "companyName": "Angela Superfacial Clinic",
+    "category": "Spa, Other"
+  },
+  {
+    "suite": 46,
+    "ownerFirst": "Stefanie",
+    "ownerLast": "Kaufman",
+    "email": "info@stefhairstylist.com",
+    "phone": "647-290-4163",
+    "companyName": "Stef.hairstylist",
+    "category": "Salon, Barber"
+  },
+  {
+    "suite": 47,
+    "ownerFirst": "Malina",
+    "ownerLast": "Hem",
+    "email": "malinahem@yahoo.ca",
+    "phone": "416-457-7616",
+    "companyName": "Hair By Malina",
+    "category": "Salon"
+  },
+  {
+    "suite": 50,
+    "ownerFirst": "Joya",
+    "ownerLast": "Salvadori",
+    "email": "info@hbsbeautyspa.com",
+    "phone": "647-328-5324",
+    "companyName": "HBS Beauty Spa",
+    "category": "Spa, Other"
+  },
+  {
+    "suite": 51,
+    "ownerFirst": "Antonietta",
+    "ownerLast": "Falcone",
+    "email": "antonietta_falcone@hotmail.com",
+    "phone": "416-707-0067",
+    "companyName": "Lavender Brow & Beauty",
+    "category": "Spa, Other"
+  }
+]
+
 
 
 function init() {
-    Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vTNik9F_6vPbPKCGneA6qMgExrkJhyl6MrzJhVj8PrK_T_EqqrFsgFJtPALhGkSw6mnX_nSuhknqZ0z/pub?output=csv`, {
+  Papa.parse(public_spreadsheet_url_suites, {
     download: true,
     header: true,
-    complete: function(results) {
-      var data = results.data
-      console.log(data)
-    }
+    complete: showInfo
   })
 }
-window.addEventListener('DOMContentLoaded', init)
+
+//window.addEventListener('DOMContentLoaded', init)
+
+function showInfo(results) {
+  const data = results.data
+
+  // data comes through as a simple array since simpleSheet is turned on
+  //alert("Successfully processed " + data.length + " rows!")
+  //document.getElementById("listing").innerHTML = "<strong>Categories:</strong> " + [ data[0].category, data[1].category, data[2].category ].join(", ");
+  console.table(data);
+}
+
+function parse() {
+  var category = [];
+  for (var i = 0; i < masterData.length; i++) {
+    category.push(masterData[i].category);
+    addCategory(i);
+  }
+  for (var i = 0; i < data.length; i++) {
+    fillCategory(i);
+  }
+};
+
+function fillCategory(i) {
+  const { category, companyName, suite } = data[i];
+  const catList = category.replace(/\s/g,'').split(',');
+  for (var j = 0; j < catList.length; j++) {
+    const newDiv = document.createElement("p");
+    newDiv.setAttribute('class', 'tenant');
+    //newDiv.setAttribute('id', category)
+    const text = document.createTextNode(`${companyName} Suite ${suite}`);
+    newDiv.appendChild(text);
+    const element = document.getElementById(catList[j]);
+    element.appendChild(newDiv);
+  }
+}
+
+function addCategory(i) {
+  const {category, color} = masterData[i]
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute('class', 'box');
+  newDiv.setAttribute('id', category)
+  const element = document.getElementById("directory");
+  element.appendChild(newDiv);
+
+  const titleDiv = document.createElement("title");
+  titleDiv.setAttribute('class', 'circle');
+  titleDiv.setAttribute('id', `${category}Title`)
+  const text = document.createTextNode(category);
+  titleDiv.appendChild(text);
+  const elementTitle = document.getElementById(category);
+  elementTitle.appendChild(titleDiv);
+  const circ = document.querySelector('title:before');
+}
+
+//window.addEventListener('DOMContentLoaded', parse);
+
+parse();
